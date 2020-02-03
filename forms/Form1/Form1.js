5472c09a6efd55a90521b141327b73a926fea640
JBenne98 <jgb83474@creighton.edu>
2020-02-03 14:57:31 -0600

    Added alert

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,9 @@
+let name='Mary'
+let names=['Mary', 'Bob', 'John']
+
+
+btnPrimary.onclick=function(){
+  console.log(`${names[2]} is here`);
+  alert(`The first person in the array is ${names[0]}`);
+}
+
